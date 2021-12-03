e02e8ae5209df4b2e23b3d67c07494c24022c9bc
annarose2255 <daytonkaroll@outlook.com>
2021-12-03 12:53:49 -0500

    Added print role method and outline of printAllRoles (may change)

--- CommonClassLibrary/Printer.cs
+++ CommonClassLibrary/Printer.cs
@@ -38,14 +38,39 @@ namespace CommonClassLibrary
             Console.WriteLine("Password: " + user.Password);
             if (user.RoleID != null)
             {
-                Console.WriteLine("Role: " ); //need to add function to get role
+                Console.WriteLine("Role: " ); //need to add function to get role then call the print role method
             }
             else
             {
                 Console.WriteLine("Role: Not Set/Added");
             }
             Console.WriteLine("----------------------------");
+        }
 
+        //NEED TO WRITE WHEN HAVE ROLES LIST 
+        public void printAllRoles(List<RoleDTO> rolesList) 
+        {
+            foreach (RoleDTO role in rolesList)
+            {
+                printRole(role);
+            }
+        } 
+
+        /// <summary>
+        /// Method to print a given role's (<paramref name="role"/>) name and description
+        /// </summary>
+        /// <param name="role">The role to print</param>
+        public void printRole(RoleDTO role)
+        {
+            Console.WriteLine("Role Name: "+role.RoleName);
+            if (role.RoleDescription != null)
+            {
+                Console.WriteLine("Role Description: "+role.RoleDescription);
+            }
+            else
+            {
+                Console.WriteLine("Role Description: Not Defined");
+            }
         }
     }
 }
