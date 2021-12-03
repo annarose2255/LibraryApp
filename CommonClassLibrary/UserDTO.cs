03fe6a36401014a64f14295e6482609339f852ee
annarose2255 <daytonkaroll@outlook.com>
2021-12-03 12:35:56 -0500

    Finished UserDTO class (for now)

--- CommonClassLibrary/UserDTO.cs
+++ CommonClassLibrary/UserDTO.cs
@@ -12,7 +12,48 @@ namespace CommonClassLibrary
         /// The ID of the user. This is the Primary key and should be unique (required)
         /// </summary>
         public int UserId { get; set; } //primary key
+        /// <summary>
+        /// First name of user
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Last name of user
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Username of user - part of login 
+        /// </summary>
+        public string UserName { get; set; } //user name of user 
+        /// <summary>
+        /// Password for user. Part of login
+        /// </summary>
+        public string Password { get; set; } //password for user
+        /// <summary>
+        /// RoleID. Forgien key to RoleDTO
+        /// </summary>
+        public int RoleID { get; set; }
 
+        //constuctors
+        public UserDTO()
+        {
+            UserId = 0;
+            UserName = "";
+            Password = "";
+        }
+        public UserDTO(int userid, string username, string password)
+        {
+            UserId =userid;
+            UserName =username;
+            Password =password;
+        }
+        public UserDTO(int userid, string username, string password, string firstname, string lastname)
+        {
+            UserId = userid;
+            UserName = username;
+            Password = password;
+            FirstName = firstname;
+            LastName = lastname;
+        }
 
     }
 }
